477cbc1bb813c64720ff061bc9b09483995aa61e
nikit34 <permikov134@mail.ru>
2020-08-25 22:50:25 +0500

    deploy editor in dj admin

--- blog/main/admin.py
+++ blog/main/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Post, BlogComment
 
 
-admin.site.register((Post, BlogComment))
+admin.site.register((BlogComment))
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('/static/js/tinyInject.js',)
